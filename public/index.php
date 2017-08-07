0afd53caf3ddd7fd772d7333d850db275a34f1d1
CodeNathan <codenathan@codenathan.com>
2017-08-07 22:31:44 +0100

    added constants and made all requests go through index.php

--- public/index.php
+++ public/index.php
@@ -3,4 +3,28 @@
 require __DIR__.'/../vendor/autoload.php';
 
 
-$app = new \App\Handler();
+$root = realpath(__DIR__.'/../');
+
+
+/*
+*
+* CONSTANTS;
+* */
+defined('DS')          or define('DS',DIRECTORY_SEPARATOR);
+defined('STORE_DEBUG') or define('STORE_DEBUG', true);
+defined('STORE_VIEWS') or define('STORE_VIEWS', $root.DS.'views');
+
+
+if(STORE_DEBUG){
+    ini_set('display_startup_errors',1);
+    ini_set('display_errors',1);
+    error_reporting(-1);
+}
+
+//$storage = new \App\Services\DatabaseStorage();
+$storage = new \App\Services\FileStorage();
+
+$app = (new \App\Handler($storage))->handle();
+
+
+
