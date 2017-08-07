09a0461fdd9873250513db285baaa809b3702f16
CodeNathan <codenathan@codenathan.com>
2017-08-07 20:53:50 +0100

    API Response complete

--- app/Core/ApiResponse.php
+++ app/Core/ApiResponse.php
@@ -3,21 +3,29 @@ namespace App\Core;
 
 class ApiResponse{
 
-    const HTTP_OK = 200;
-    const HTTP_METHOD_NOT_ALLOWED = 405;
-    const HTTP_NOT_FOUND = 404;
+    const HTTP_OK                       = 200; // OK - Everything went well
+    const HTTP_BAD_REQUEST              = 400; // Bad Request - Server does not understand what we mean
+    const HTTP_METHOD_NOT_ALLOWED       = 405; // Method Not Allowed - Trying a get on a post method
+    const HTTP_NOT_FOUND                = 404; // NOT FOUND - The request methods do not exist
+
 
     /**
+     * Used to see if the request is success or not
+     *
      * @var boolean
      */
     public $success = false;
 
     /**
-     * @var null
+     * Response data
+     *
+     * @var null | array | string
      */
     public $response = null;
 
     /**
+     * The status code as a result of request
+     *
      * @var int
      */
     public $status_code = 404;
