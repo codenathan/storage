2659633dd34ddb5d2e905e5646c1416859c4d832
CodeNathan <codenathan@codenathan.com>
2017-08-11 16:59:16 +0100

    completed database storage functionality except for create function

--- app/Services/DatabaseStorage.php
+++ app/Services/DatabaseStorage.php
@@ -2,6 +2,7 @@
 
 namespace  App\Services;
 
+use App\Core\ApiResponse;
 use App\Core\Storage;
 use App\Interfaces\iStorage;
 
@@ -13,7 +14,12 @@ class DatabaseStorage extends Storage implements iStorage{
      */
     public static $instance;
 
+    public $query;
+
     public $statement;
+
+    public $query_errors = array();
+
 
     public static function getInstance(){
 
@@ -23,8 +29,10 @@ class DatabaseStorage extends Storage implements iStorage{
                 self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
                 self::$instance->query('SET NAMES utf8');
                 self::$instance->query('SET CHARACTER SET utf8');
-            }catch(\PDOException $error) {
-                echo $error->getMessage();
+            }catch(\PDOException $e) {
+                $error = null;
+                if(STORE_DEBUG) $error = $e->getMessage();
+                return self::sendConnectionErrorResponse($error);
 
             }
         }
@@ -32,62 +40,147 @@ class DatabaseStorage extends Storage implements iStorage{
         return self::$instance;
     }
 
-
     public function index()
     {
-        $query = "SELECT * FROM ".$this->model->getModelName();
-        $this->statement = self::getInstance()->prepare($query);
-        $this->statement->execute();
+        try{
+            $this->query = "SELECT * FROM ".$this->model->getModelName();
+            return $this->executeAndFetch();
+
+        }catch (\PDOException $error){
+            return $this->sendDatabaseErrorResponse($error);
+
+        }
     }
 
     public function save($create = false)
     {
+        if($create){
+            $this->setCreateFields();
+        } else{
+            $this->setUpdateFields();
+        }
+
+        $instance = self::getInstance();
+        if(!$instance instanceof \PDO) return $instance;
+
         $properties = $this->model->properties();
-        unset($properties['ID']);
+        $key = array_search('ID', $properties);
+
+        unset($properties[$key]);
 
         if($create){
+            try{
+                $question_marks = array_map(function($val) { return "?"; }, $properties);
 
-            $question_marks = array_map(function($val) { return "?"; }, $properties);
+                $query = "INSERT INTO (".implode(",",$properties).") VALUES (".implode(",",$question_marks)." ) ";
 
-            $query = "INSERT INTO (".implode(",",$properties).") VALUES (".implode(",",$question_marks)." ) ";
-            $this->statement = self::getInstance()->prepare($query);
 
-            $i = 1;
-            foreach($properties as $property){
-                $this->statement->bindParam($i,$this->model->{$property});
+                $this->statement = self::getInstance()->prepare($query);
+
+                $i = 1;
+                $this->query_errors['query'] = $query;
+                $this->query_errors['properties'] = $properties;
+                $this->query_errors['questions'] = $question_marks;
+
+                foreach($properties as $property){
+                    if(!isset($this->query_errors['model'])){
+                        $this->query_errors['model'] = [];
+                    }
+                    $this->query_errors['model'][$property]  = [
+                        $i, $this->model->{$property}
+                    ];
+
+                    $this->statement->bindParam($i,$this->model->{$property});
+                }
+
+                return $this->testQueryErrors();
+
+                $this->statement->execute();
+            }catch (\PDOException $error){
+                return $this->sendDatabaseErrorResponse($error);
             }
-
-            $this->statement->execute();
 
 
         }else{
-
-            $query =  "UPDATE ".$this->model->getModelName()." SET "." "." WHERE ID = (:ID)";
-            $this->statement = self::getInstance()->prepare($query);
-            $this->statement->bindParam(':ID',$this->model->ID,\PDO::PARAM_INT);
-            $this->statement->execute();
+            try {
+                $this->query =  "UPDATE ".$this->model->getModelName()." SET "." "." WHERE ID = (:ID)";
+                return $this->executeAndFetch(true);
+            }catch (\PDOException $error){
+                return $this->sendDatabaseErrorResponse($error);
+            }
         }
 
     }
 
     public function find()
     {
-        $query = "SELECT * FROM ".$this->model->getModelName()." WHERE ID = (:ID)";
-        $this->statement = self::getInstance()->prepare($query);
-        $this->statement->bindParam(':ID',$this->model->ID,\PDO::PARAM_INT);
-        $this->statement->execute();
-        $this->statement->fetchAll(\PDO::FETCH_ASSOC);
+        $instance = self::getInstance();
+        if(!$instance instanceof \PDO) return $instance;
+
+        try {
+            $this->query = "SELECT * FROM " . $this->model->getModelName() . " WHERE ID = (:ID)";
+            return $this->executeAndFetch(true);
+
+            return $this->returnSuccessResponse($data);
+        }catch (\PDOException $error){
+            return $this->sendDatabaseErrorResponse($error);
+        }
     }
 
     public function delete()
     {
-        $query = "DELETE FROM ".$this->model->getModelName()." WHERE ID = (:ID)";
-        $this->statement = self::getInstance()->prepare($query);
-        $this->statement->bindParam(':ID',$this->model->ID,\PDO::PARAM_INT);
+        $instance = self::getInstance();
+        if(!$instance instanceof \PDO) return $instance;
+        try {
+            $this->query = "DELETE FROM ".$this->model->getModelName()." WHERE ID = (:ID)";
+            return $this->executeAndFetch(true,true);
+
+        }catch (\PDOException $error){
+            return $this->sendDatabaseErrorResponse($error);
+        }
+    }
+
+    private function executeAndFetch($bind_ID = false,$delete = false){
+
+        $instance = self::getInstance();
+        if(!$instance instanceof \PDO) return $instance;
+
+        $this->statement = self::getInstance()->prepare($this->query);
+
+        if($bind_ID){
+            $this->statement->bindParam(':ID',$this->model->ID,\PDO::PARAM_INT);
+        }
+
         $this->statement->execute();
+
+        $data = $delete ? true : $this->statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $this->returnSuccessResponse($data);
+    }
+
+    private function sendDatabaseErrorResponse(\PDOException $error){
+        $errors = [];
+
+        if(STORE_DEBUG){
+            $errors['query']        = $this->query;
+            $errors['query_error']  = $error->getMessage();
+        }
+
+        return $this->returnErrorResponse($errors);
+    }
+
+    private function testQueryErrors(){
+        return $this->returnErrorResponse($this->query_errors);
     }
 
 
+    public function getNextAvailableID()
+    {
+        // TODO: Implement getNextAvailableID() method.
+    }
 
-
+    public function getAllData()
+    {
+        // TODO: Implement getAllData() method.
+    }
 }
